1efbf1184ffd3e05a0380dd01678dad8faa48e5f
guilleatm <guillearnautm@gmail.com>
2024-06-08 09:21:20 +0100

    layout

--- src/main.js
+++ src/main.js
@@ -4,10 +4,13 @@ window.onload = onWindowLoaded;
 async function onWindowLoaded()
 {
 
-	await loadHtml("pages/indie_summit.html");
+	await loadHtml("pages/about.html");
 
 	let tve_link = document.getElementById('tve_link');
 	tve_link.onclick = () => loadHtml("pages/tve.html");
+
+	let about_link = document.getElementById('about_link');
+	about_link.onclick = () => loadHtml("pages/about.html");
 
 	let indie_summit_link = document.getElementById('indie_summit_link');
 	indie_summit_link.onclick = () => loadHtml("pages/indie_summit.html");
